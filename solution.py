89087e9518068d410cb8e9dafffb5b0ae152caf6
xsitin <m9ckom@gmail.com>
2023-04-02 14:02:55 +0500

    first try

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import lognorm
+from scipy.optimize import minimize_scalar
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 385459798 # Ваш chat ID, не меняйте название переменной
 
+
+
+
+def log_likelihood(a, x):
+    sigma = np.sqrt(np.var(np.log(x)) + np.mean(np.log(x)) ** 2)
+    return -np.sum(lognorm.logpdf(x, s=sigma, scale=np.exp(a)))
+
+    
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    result = minimize_scalar(log_likelihood, args=(x,))
+    return result.x
