2f86b03dbfaa59d717fa580d6c76e7865b25ac02
xsitin <m9ckom@gmail.com>
2023-04-03 22:42:00 +0500

    third try

--- solution.py
+++ solution.py
@@ -1,15 +1,13 @@
+import math
 import pandas as pd
 import numpy as np
 from scipy.stats import lognorm
 from scipy.optimize import minimize_scalar
+from scipy.optimize import minimize
 
 
-chat_id = 385459798 # Ваш chat ID, не меняйте название переменной
+chat_id = 385459798  # Ваш chat ID, не меняйте название переменной
 
 
-    
 def solution(x: np.array) -> float:
-    x = np.array([v - 855 for v in x])
-    print(x)
-    s, loc, scale = lognorm.fit(x)
-    return loc;
+    return np.log(x-855).mean()
